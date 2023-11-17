6e349f53b782614fa7249eef00b53908ae55423d
clemzouuu <clemkinoa@gmal.com>
2023-11-17 23:25:21 +0100

    adding better sheeps

--- src/index3.js
+++ src/index3.js
@@ -1,9 +1,10 @@
-import { render, lightBrown, grey,lightGreen } from "./color.js";
+import { render, lightBrown, grey,lightGreen,white,black } from "./color.js";
 import * as drawlib from "./drawlib.js";
 
 const tailleArbre = 70;
-const tailleMouton = 20;
-const tailleFeuilles = 70
+const tailleMouton = 30;
+const tailleFeuilles = 70;
+const yeuxMoutons = 5;
 /**
  * @param {CanvasRenderingContext2D} context
  * @param {number} x
@@ -40,6 +41,28 @@ function afficherMouton(context, x, y) {
   drawlib.renderCentered(drawlib.move(x, y, mouton), context);
 }
 
+/**
+ * @param {CanvasRenderingContext2D} context
+ * @param {number} x
+ * @param {number} y
+ */
+function afficherYeuxMouton(context, x, y) {
+  const couleurMouton = black; 
+  const mouton = drawlib.circle(couleurMouton, yeuxMoutons);
+  drawlib.renderCentered(drawlib.move(x, y, mouton), context);
+}
+
+/**
+ * @param {CanvasRenderingContext2D} context
+ * @param {number} x
+ * @param {number} y
+ */
+function afficherTeteMouton(context, x, y) {
+  const couleurMouton = white; 
+  const mouton = drawlib.circle(couleurMouton, tailleMouton);
+  drawlib.renderCentered(drawlib.move(x, y, mouton), context);
+}
+
 function afficherDessin() {
   const canvas = document.getElementById("myCanvas");
   
@@ -58,10 +81,28 @@ function afficherDessin() {
       afficherTronc(context, 300, -100);
       afficherFeuilles(context, 300, -160)
 
-      afficherMouton(context, 200, 200);
-      afficherMouton(context, 200, 200);
-      afficherMouton(context, -100, 100);
-      afficherMouton(context, 0, -100);
+      afficherMouton(context, 200, 45);
+      afficherMouton(context, 160, 80); 
+      afficherMouton(context, 240, 80); 
+      afficherTeteMouton(context, 200, 80);
+      afficherYeuxMouton(context, 200, 80);
+      afficherYeuxMouton(context, 180, 80);
+
+      
+      afficherMouton(context, -70, 50);
+      afficherMouton(context, -140, 50);
+      afficherMouton(context, -100, 20);
+      afficherTeteMouton(context, -110, 60);
+      afficherYeuxMouton(context, -110, 60);
+      afficherYeuxMouton(context, -130, 60);
+
+
+      afficherMouton(context, 30, -110);
+      afficherMouton(context, 0, -150);
+      afficherMouton(context, -30, -110);
+      afficherTeteMouton(context, 0, -110);
+      afficherYeuxMouton(context, 10, -100);
+      afficherYeuxMouton(context, -10, -100);
     }
   }
 }
